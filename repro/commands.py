18cada149b71e50954f6c6b49d692f9839089880
Vince Bickers <vince@graphaware.com>
2019-01-10 11:20:37 +0000

    Fixes (all?) neomodel cluster issues

--- repro/commands.py
+++ repro/commands.py
@@ -7,11 +7,11 @@ from loguru import logger
 
 db = neomodel.db
 neomodel.config.DATABASE_URL = "bolt+routing://test:test@neo4j-0:7687"
+
 READ_QUERY = "MATCH (n) RETURN n"
 INSERT_QUERY = "CREATE (p:Person)-[:LIKES]->(t:Technology)"
 CLUSTER_QUERY_URL = f"http://neo4j-0:7474/db/data/transaction/commit"
 CLUSTER_QUERY_STATEMENT = r"CALL dbms.cluster.overview()"
-
 
 def parse_neo4j_status(resp):
     out = []
@@ -19,56 +19,71 @@ def parse_neo4j_status(resp):
         out.append((row[1][0][7:14], row[2]))
     return out
 
-
 def retrieve_cluster_info():
-    resp = r.post(CLUSTER_QUERY_URL, json={"statements": [{"statement": CLUSTER_QUERY_STATEMENT}]})
-    return parse_neo4j_status(resp.json())
-
+    try:
+        resp = r.post(CLUSTER_QUERY_URL, json={"statements": [{"statement": CLUSTER_QUERY_STATEMENT}]})
+        return parse_neo4j_status(resp.json())
+    except r.exceptions.ConnectionError:
+        logger.info("The neo4j-0 instance is not available. Perhaps the cluster is still starting up or reforming after a leader election")
 
 def print_cluster_info():
     logger.info(f"{retrieve_cluster_info()}")
 
-
 def retrieve_cluster_leader():
-    for server, role in retrieve_cluster_info():
-        if role == "LEADER":
-            return server
+    cluster_info = retrieve_cluster_info()
+    if cluster_info:
+        for server, role in cluster_info:
+            if role == "LEADER":
+                return server
 
-
-def read_data():
-    return db.cypher_query(READ_QUERY)
-
-
-@db.write_transaction
-def write_data():
-    return db.cypher_query(INSERT_QUERY)
-
-
-def do_first():
-    logger.info(f"Reading data: {read_data()}")
-    logger.info(f"Writing data")
-    write_data()
-    logger.info("Successfully wrote data")
-    logger.info(f"Reading data: {read_data()}")
-    logger.info(f"Please restart the leader using this command: docker-compose restart {retrieve_cluster_leader()}")
-
-
-def do_second():
+def do_request(request):
     while True:
         try:
-            data = read_data()
-            logger.info(f"Reading data: {data}")
-            break
+            return db.cypher_query(request)
         except neo4j.v1.exceptions.SessionExpired:
-            logger.info(f"Hit SessionExpired...retrying")
+            logger.info("Session has expired. Trying again in 1 second")
             time.sleep(1)
             continue
+        except neo4j.exceptions.ServiceUnavailable:
+            logger.info("Neo4j is unavailable. Trying again in 5 seconds")
+            time.sleep(5)
+            continue
+        except neo4j.exceptions.NotALeaderError:
+            logger.info("Routing table is incorrect. Attempting to locate and connect to new leader")
+            reconnect()
+            continue
+        except:
+            raise
 
-    try:
-        logger.info(f"Attempting to write data")
-        write_data()
-        logger.info("Successfully wrote data")
-    except neo4j.exceptions.NotALeaderError as e:
-        logger.error(traceback.format_exc())
+def reconnect():
+    while True:
+        cluster_leader = retrieve_cluster_leader()
+        if cluster_leader:
+            logger.info(f"Got new leader {cluster_leader}")
+            return reconnect_to_leader(cluster_leader)
+        logger.info("Could not locate leader. Trying again in 5 seconds")
+        time.sleep(5)
 
+def reconnect_to_leader(cluster_leader):
 
+      url = "bolt+routing://test:test@" + cluster_leader + ":7687"
+
+      while True:
+          try:
+              db.set_connection(url)
+              logger.info(f"Connected to new leader {url}")
+              break
+          except neo4j.exceptions.ServiceUnavailable:
+              logger.info("Waiting for routing information to become available...")
+              time.sleep(5)
+              continue
+
+def write():
+    logger.info("Attempting to write data")
+    do_request(INSERT_QUERY)
+    logger.info("Successfully wrote data")
+
+def read():
+    logger.info("Attempting to read data")
+    data = do_request(READ_QUERY)
+    logger.info(f"Successfully read data {data}")
